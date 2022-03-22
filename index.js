35097a4b09f68f3ec6757e56e07fc4d5bb0f1e4c
Griffin <gmd298@gmail.com>
2022-03-22 12:34:04 -0400

    commit tree 05677b730316d1e2b16be51226462ddffee7947e

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
@@ -8,7 +9,10 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = book.numberOfPages;
     main.appendChild(h2);
+    main.appendChild(paragraph);
   });
 }
 
